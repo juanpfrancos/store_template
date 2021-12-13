857cd23572674c442e3bbaa803a829cf2d498abb
Juan Pablo <juanpablo.franco@afkar.digital>
2021-12-12 20:56:30 -0500

    Adding GlassMagnifier.

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,26 +1,44 @@
+import React, { useState, useEffect } from "react";
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
 import Grid from '@mui/material/Grid';
-import React, { useState, useEffect } from "react";
+import {GlassMagnifier} from "react-image-magnifiers";
 import dataJson from '../static/zapato.json'
 
 export default function Products() {
     const [data, setData] = useState(dataJson);
+    const [selected, setSelected] = useState();
+    const [hide, setHide] = useState(false);
 
+    const onRowClick = nombre => () => {
+        const index = data.findIndex(data => data.nombre === nombre);
+        const row = data[index]
+        setSelected(data[index]) /*Añade al estado la fila seleccionada*/
+        setHide(true);
+    }
   return (
     <Grid container spacing={2}>
+        {hide && 
+            (<GlassMagnifier
+              imageSrc={`https://raw.githubusercontent.com/juanpfrancos/store_template/main/src/static/img/${selected.foto}`}
+              imageAlt="Example"
+              largeImageSrc={`https://raw.githubusercontent.com/juanpfrancos/store_template/main/src/static/img/${selected.foto}`}
+            />)
+        }
+
+
         {
            data.map( item => (
                 <Grid item xs={3} md={3} lg={3}>
                     <Card sx={{ maxWidth: 300 }}>
-                        <CardActionArea>
+                        <CardActionArea onClick={onRowClick(item.nombre)}>
                             <CardMedia
                                 component="img"
-                                height="140"
-                                image="https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/d5f168f9-f953-4419-ac7a-f0def128176e/renew-run-2-road-running-shoe-z8BVXH.png"
+                                height="200"
+                                image={`https://raw.githubusercontent.com/juanpfrancos/store_template/main/src/static/img/${item.foto}`}
                                 alt={item.nombre}
                             />
                             <CardContent>
