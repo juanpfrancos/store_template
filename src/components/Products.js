17e3cb490484116cbf977ac5c172e86bd7a4c8e0
Juan Pablo <juanpablo.franco@afkar.digital>
2021-12-13 17:53:32 -0500

    Quitting grids.

--- src/components/Products.js
+++ src/components/Products.js
@@ -31,101 +31,107 @@ export default function Products() {
         setHide(true);
     }
   return (
-    <Grid container spacing={2}
-          direction="row"
-          justifyContent="center"
-          alignItems="center"
-    >
-        {hide && 
-            (
-                <Grid container spacing={2} direction="row" justifyContent="center" alignItems="center">
-                    <Grid item xs={10} md={4} lg={3}>
-                        <GlassMagnifier
-                            imageSrc={`https://raw.githubusercontent.com/juanpfrancos/store_template/main/src/static/img/r${selected.foto}`}
-                            imageAlt={selected.referencia}
-                            largeImageSrc={`https://raw.githubusercontent.com/juanpfrancos/store_template/main/src/static/img/${selected.foto}`}
-                        />
-                    </Grid>
-                    <Grid item xs={10} md={4} lg={3}>
-                        <Box sx={{ width: '100%'}}>
-                        <Typography variant="h3" gutterBottom component="div">
-                            {selected.nombre}
-                        </Typography>
-                        <Typography variant="h4" gutterBottom component="div">
-                        $ {selected.precio}
-                        </Typography>
-                        <Typography variant="body1" gutterBottom>
-                            Referencia: {selected.referencia}
-                        </Typography>
-                        <Typography variant="h4" gutterBottom>
-                            Tallas
-                        </Typography>
-                            <Grid container spacing={2}
-                                  direction="row"
-                                  justifyContent="flex-star"
-                                  alignItems="center"
-                            >
-                                <Grid item xs={2}>
-                                <Item>5</Item>
-                                </Grid>
-                                <Grid item xs={2}>
-                                <Item>5.5</Item>
-                                </Grid>
-                                <Grid item xs={2}>
-                                <Item>6</Item>
-                                </Grid>
-                                <Grid item xs={2}>
-                                <Item>6.5</Item>
-                                </Grid>
-                                <Grid item xs={2}>
-                                <Item>7</Item>
-                                </Grid>
-                                <Grid item xs={2}>
-                                <Item>7.5</Item>
-                                </Grid>
-                                <Grid item xs={2}>
-                                <Item>8</Item>
-                                </Grid>
-                                <Grid item xs={2}>
-                                <Item>8.5</Item>
-                                </Grid>
-                            </Grid>
-                            <br/>
-                            <Button variant="outlined" startIcon={<AddShoppingCartIcon />}>Añadir al carrito</Button>
-                        </Box>
+    <>
+        <Typography variant="h4" gutterBottom>
+            Promociones
+        </Typography>
+        <Grid container spacing={2}
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+        >
 
-                    </Grid>
-                </Grid>
-            )
-        }
-
-
-        {
-           data.map( item => (
-                <Grid item xs={8} md={4} lg={3} key={item.nombre}>
-                    <Card sx={{ maxWidth: 300 }}>
-                        <CardActionArea onClick={onRowClick(item.nombre)}>
-                            <CardMedia
-                                component="img"
-                                height="200"
-                                image={`https://raw.githubusercontent.com/juanpfrancos/store_template/main/src/static/img/r${item.foto}`}
-                                alt={item.nombre}
+            {hide && 
+                (
+                    <Grid container spacing={2} direction="row" justifyContent="center" alignItems="center">
+                        <Grid item xs={10} md={4} lg={3}>
+                            <GlassMagnifier
+                                imageSrc={`https://raw.githubusercontent.com/juanpfrancos/store_template/main/src/static/img/r${selected.foto}`}
+                                imageAlt={selected.referencia}
+                                largeImageSrc={`https://raw.githubusercontent.com/juanpfrancos/store_template/main/src/static/img/${selected.foto}`}
                             />
-                            <CardContent>
-                            <Typography gutterBottom variant="h5" component="div">
-                                {item.nombre}
+                        </Grid>
+                        <Grid item xs={10} md={4} lg={3}>
+                            <Box sx={{ width: '100%'}}>
+                            <Typography variant="h3" gutterBottom component="div">
+                                {selected.nombre}
                             </Typography>
-                            <Typography gutterBottom variant="h5" component="div">
-                               $ {item.precio}
+                            <Typography variant="h4" gutterBottom component="div">
+                            $ {selected.precio}
                             </Typography>
-                            <Typography variant="body2" color="text.secondary">
-                               Referencia: {item.referencia}
+                            <Typography variant="body1" gutterBottom>
+                                Referencia: {selected.referencia}
                             </Typography>
-                            </CardContent>
-                        </CardActionArea>
-                    </Card>
-                </Grid>
-            ))
-        }
-    </Grid>     
+                            <Typography variant="h4" gutterBottom>
+                                Tallas
+                            </Typography>
+                                <Grid container spacing={2}
+                                    direction="row"
+                                    justifyContent="flex-star"
+                                    alignItems="center"
+                                >
+                                    <Grid item xs={2}>
+                                    <Item>5</Item>
+                                    </Grid>
+                                    <Grid item xs={2}>
+                                    <Item>5.5</Item>
+                                    </Grid>
+                                    <Grid item xs={2}>
+                                    <Item>6</Item>
+                                    </Grid>
+                                    <Grid item xs={2}>
+                                    <Item>6.5</Item>
+                                    </Grid>
+                                    <Grid item xs={2}>
+                                    <Item>7</Item>
+                                    </Grid>
+                                    <Grid item xs={2}>
+                                    <Item>7.5</Item>
+                                    </Grid>
+                                    <Grid item xs={2}>
+                                    <Item>8</Item>
+                                    </Grid>
+                                    <Grid item xs={2}>
+                                    <Item>8.5</Item>
+                                    </Grid>
+                                </Grid>
+                                <br/>
+                                <Button variant="outlined" startIcon={<AddShoppingCartIcon />}>Añadir al carrito</Button>
+                            </Box>
+
+                        </Grid>
+                    </Grid>
+                )
+            }
+
+
+            {
+            data.map( item => (
+                    <Grid item xs={8} md={4} lg={3} key={item.nombre}>
+                        <Card sx={{ maxWidth: 300 }}>
+                            <CardActionArea onClick={onRowClick(item.nombre)}>
+                                <CardMedia
+                                    component="img"
+                                    height="200"
+                                    image={`https://raw.githubusercontent.com/juanpfrancos/store_template/main/src/static/img/r${item.foto}`}
+                                    alt={item.nombre}
+                                />
+                                <CardContent>
+                                <Typography gutterBottom variant="h5" component="div">
+                                    {item.nombre}
+                                </Typography>
+                                <Typography gutterBottom variant="h5" component="div">
+                                $ {item.precio}
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                Referencia: {item.referencia}
+                                </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                        </Card>
+                    </Grid>
+                ))
+            }
+        </Grid>    
+     </> 
     )}
